1d5e1c6a3b81e201cf1d97a152eb7fe5550acce4
Guillaume Grossetie <ggrossetie@gmail.com>
2022-11-29 21:46:56 +0100

    pass simple fixture

--- libasciidoc_tck.go
+++ libasciidoc_tck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"github.com/bytesparadise/libasciidoc/pkg/configuration"
 	"github.com/bytesparadise/libasciidoc/pkg/types"
@@ -9,19 +10,49 @@ import (
 	"os"
 )
 
+type AbstractSemanticGraph types.Document
+type Paragraph types.Paragraph
+
+func (p Paragraph) MarshalJSON() ([]byte, error) {
+	m := make(map[string]interface{})
+	elements := p.Elements
+	if s, ok := elements[0].(*types.StringElement); ok {
+		m["type"] = "Paragraph"
+		m["lines"] = []string{s.Content}
+	}
+	return json.Marshal(m)
+}
+
+func (p AbstractSemanticGraph) MarshalJSON() ([]byte, error) {
+	m := make(map[string]interface{})
+	var elements []interface{}
+	for _, element := range p.Elements {
+		if _, isAttributeDeclaration := element.(*types.AttributeDeclaration); isAttributeDeclaration {
+			continue
+		}
+		if para, isParagraph := element.(*types.Paragraph); isParagraph {
+			elements = append(elements, Paragraph{
+				Attributes: para.Attributes,
+				Elements:   para.Elements,
+			})
+		} else {
+			elements = append(elements, element)
+		}
+	}
+	m["body"] = elements
+	return json.Marshal(m)
+}
+
 func main() {
 	input := bufio.NewReader(os.Stdin)
 	//output := bufio.NewWriter(os.Stdout)
 	doc, _ := pkg.Parse(input, configuration.NewConfiguration(configuration.WithHeaderFooter(true)))
-	for _, element := range doc.Elements {
-		v, ok := element.(*types.Section)
-		if ok {
-			fmt.Printf("%i", v.Level)
-			fmt.Printf("%s", v.Title)
-		}
-
-		fmt.Printf("%v\n", v)
-		fmt.Printf("%v\n", element)
-		fmt.Printf("%T\n", element)
+	asg := AbstractSemanticGraph{
+		Elements:          doc.Elements,
+		ElementReferences: doc.ElementReferences,
+		Footnotes:         doc.Footnotes,
+		TableOfContents:   doc.TableOfContents,
 	}
+	byteSlice, _ := json.Marshal(asg)
+	fmt.Println(string(byteSlice))
 }
